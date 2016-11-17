b4fcfd4bed148c254c867cf73420754b55f13b08
Guillaume Martin <guillaume.martin@share-gate.com>
2016-11-16 20:08:32 -0500

    Remove react, added what is atomable/what can atomable do for me texts.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './App';
-import './index.css';
-
-ReactDOM.render(
-  <AppRouter />,
-  document.getElementById('root')
-);
